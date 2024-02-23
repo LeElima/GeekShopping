c51b284f35a20610b57ca626f4fe7ca2c337fe78
Leandro de Lima <leandrolima@cobrape.com.br>
2024-02-23 06:47:10 -0300

    Criação dos arquivos para o Cart

---
+++ GeekShopping.CartAPI/Model/Context/SQLContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CartAPI.Model.Context
+{
+    public class SQLContext : DbContext
+    {
+        public SQLContext(){}
+        public SQLContext(DbContextOptions<SQLContext> options) : base(options){}
+        public DbSet<CartProduct> CartProducts { get; set;}
+        public DbSet<CartDetail> CartDetails { get; set;}
+        public DbSet<CartHeader> CartHeaders { get; set;}
+    }
+}
---
+++ GeekShopping.CartAPI/Repository/CartRepository.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Model.Context;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly SQLContext _context;
+        private IMapper _mapper;
+        public CartRepository(SQLContext sQLContext, IMapper mapper)
+        {
+            _context = sQLContext;
+            _mapper = mapper;
+        }
+        public Task<bool> ApplyCoupon(string userId, string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ClearCart(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartVO> FindCartByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveCoupon(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFromCart(long cartDetailsId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartVO> SaveOrUpdateCart(CartVO cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
