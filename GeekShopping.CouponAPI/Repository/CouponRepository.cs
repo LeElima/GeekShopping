1e02136b4de0877aa3c29e8bc70e35740da97876
Leandro de Lima <leandrolima@cobrape.com.br>
2024-03-01 07:49:41 -0300

    Repository criado

---
+++ GeekShopping.CouponAPI/Repository/CouponRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace GeekShopping.CouponAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly SQLContext _context;
+        private IMapper _mapper;
+        public CouponRepository(SQLContext sQLContext, IMapper mapper)
+        {
+            _context = sQLContext;
+            _mapper = mapper;
+        }
+
+    }
+}
---
+++ GeekShopping.CouponAPI/Repository/ICouponRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace GeekShopping.CouponAPI.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<CouponVO> GetCouponByCouponCod(string couponCode);
+    }
+}
