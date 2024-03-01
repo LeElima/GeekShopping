42c5992dbf5c81f0fa238f5f690a5481243b9992
Leandro de Lima <leandrolima@cobrape.com.br>
2024-03-01 07:37:36 -0300

    Criação API e Migrations

---
+++ GeekShopping.CouponAPI/Model/Context/SQLContext.cs
@@ -0,0 +1,12 @@
+﻿using GeekShopping.CouponAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CouponAPI.Model.Context
+{
+    public class SQLContext : DbContext
+    {
+        public SQLContext(){}
+        public SQLContext(DbContextOptions<SQLContext> options) : base(options){}
+        public DbSet<Coupon> Coupon { get; set;}
+    }
+}
